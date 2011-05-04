a247452effc9f87954d03181702adcc123786c66
James Baldassari <jbaldassari@gmail.com>
2011-05-03 20:30:42 -0400

    Implemented problem 8

---
+++ src/main/scala/p08/P08.scala
@@ -0,0 +1,74 @@
+package p08
+
+import scala.annotation._
+
+/**
+ * Implementation of project 8.
+ */
+object P08 {
+	/**
+	 * Eliminate consecutive duplicates of list elements recursively.
+	 * @param list the list to compress.
+	 * @return the compressed list.
+	 */
+	def compressRecursive[A](list: List[A]): List[A] = list match {
+		case Nil => Nil
+		case head :: tail => head :: compressRecursive(tail.dropWhile(_ == head))
+	}
+	
+	/**
+	 * Eliminate consecutive duplicates of list elements tail-recursively.
+	 * @param list the list to compress.
+	 * @return the compressed list.
+	 */
+	def compressTailRecursive1[A](list: List[A]): List[A] = {
+		/**
+		 * Compresses the head of a list with the tail of the list.
+		 * @param result accumulator list for the end result.
+		 * @param head the current head being examined.
+		 * @param tail the remainder of the list to process.
+		 */
+		@tailrec
+		def compress(result: List[A], head: A, tail: List[A]): List[A] = tail match {
+			case Nil => (head :: result).reverse
+			case h :: t => if (head == h) compress(result, head, t) else compress(head :: result, h, tail)
+		}
+		list match {
+			case Nil => Nil
+			case head :: tail => compress(Nil, head, tail)
+		}
+	}
+
+	/**
+	 * Eliminate consecutive duplicates of list elements tail-recursively.
+	 * @param list the list to compress.
+	 * @return the compressed list.
+	 */
+	def compressTailRecursive2[A](list: List[A]): List[A] = {
+		/**
+		 * Compresses a list.
+		 * @param result accumulator list for the end result.
+		 * @param list the list to compress
+		 */
+		@tailrec
+		def compress(result: List[A], list: List[A]): List[A] = list match {
+			case Nil => result.reverse
+			case head :: tail => compress(head :: result, tail.dropWhile(_ == head))
+		}
+		list match {
+			case Nil => Nil
+			case elems => compress(Nil, elems)
+		}
+	}
+
+	/**
+	 * Eliminate consecutive duplicates of list elements.
+	 * @param list the list to compress.
+	 * @return the compressed list.
+	 */
+	def compressFunctional[A](list: List[A]): List[A] = list.foldRight(List[A]()) { 
+		(head, tail) => 
+			if (tail.isEmpty || (head != tail.head)) head :: tail 
+			else tail
+	}
+}
---
+++ src/test/scala/p08/P08Spec.scala
@@ -0,0 +1,23 @@
+package p08
+
+import org.specs._
+
+/**
+ * Test for problem 7.
+ */
+class P08Spec extends SpecificationWithJUnit {
+	"P08" should {
+		"eliminate consecutive duplicates of list elements recursively" in {
+			P08.compressRecursive(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) mustEqual List('a, 'b, 'c, 'a, 'd, 'e)
+		}
+		"eliminate consecutive duplicates of list elements tail-recursively" in {
+			P08.compressTailRecursive1(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) mustEqual List('a, 'b, 'c, 'a, 'd, 'e)
+		}
+		"eliminate consecutive duplicates of list elements tail-recursively" in {
+			P08.compressTailRecursive2(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) mustEqual List('a, 'b, 'c, 'a, 'd, 'e)
+		}
+		"eliminate consecutive duplicates of list elements functionally" in {
+			P08.compressFunctional(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) mustEqual List('a, 'b, 'c, 'a, 'd, 'e)
+		}
+	}
+}
